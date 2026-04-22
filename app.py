2ba10e8bc4bbef096c623c11df5f886027bca33d
Nadendla <2368167@cognizant.com>
2026-04-22 16:15:10 +0530

    commit1

--- app.py
+++ app.py
@@ -7,11 +7,29 @@ def hello():
     return """
     <html>
         <body style="font-family: Arial; text-align: center; margin-top: 100px; background-color: #f0f0f0;">
-            <h1 style="color: #0078d4;">🚀 Hello from Python Web App on Azure! Siva Nadendla</h1>
-            <p style="color: #333; font-size: 18px;">Version 2.0 - Successfully Deployed via Azure DevOps</p>
-            <p style="color: green; font-size: 16px;">✅ Pipeline is working correctly!</p>
-            <hr>
-            <p style="color: #888;">Deployed by: nadendlasiva</p>
+            
+            <h1 style="color: #0078d4;">🚀 Vivek</h1>
+
+            <div style="margin-top: 20px;">
+                <span style="background-color:#0078d4; color:white; padding:8px 14px; border-radius:20px; margin:5px; display:inline-block;">Akshay</span>
+                <span style="background-color:#0078d4; color:white; padding:8px 14px; border-radius:20px; margin:5px; display:inline-block;">Lenin</span>
+                <span style="background-color:#0078d4; color:white; padding:8px 14px; border-radius:20px; margin:5px; display:inline-block;">Siva</span>
+            </div>
+
+            <p style="color: #333; font-size: 18px; margin-top: 30px;">
+                Version 2.0 - Successfully Deployed via Azure DevOps
+            </p>
+
+            <p style="color: green; font-size: 16px;">
+                ✅ Pipeline is working correctly!
+            </p>
+
+            <hr style="width:50%; margin-top:30px;">
+
+            <p style="color: #888;">
+                Deployed by: nadendlasiva
+            </p>
+
         </body>
     </html>
     """
