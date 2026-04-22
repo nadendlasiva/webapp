c054ae09384e695adfa7ca01d89f7d0130828039
Nadendla <2368167@cognizant.com>
2026-04-22 12:45:42 +0530

    text changed

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Welcome from Python!"
+    return "Hello Welcome from Python second commit"
 
 @app.route("/health")
 def health():
